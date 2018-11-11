911a2c0649068b9fda9256cb16f761a131e46a29
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2018-11-11 15:05:01 -0500

    basicas

---
+++ db/datos-basicas.sql
@@ -0,0 +1,17 @@
+
+INSERT INTO public.aspsicosocial (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (0, 'SIN INFORMACIÓN', '2014-02-14', NULL, NULL, NULL, NULL);
+INSERT INTO public.aspsicosocial (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (1, 'ATENCIÓN PSICOLÓGICA (INDIVIDUAL, GRUPAL O FAMILIAR)', '2014-02-14', NULL, NULL, NULL, NULL);
+INSERT INTO public.aspsicosocial (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (2, 'SEGUIMIENTO A - VISITAS', '2014-02-14', NULL, NULL, NULL, NULL);
+INSERT INTO public.aspsicosocial (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (3, 'PARTICIPACIÓN', '2014-02-14', NULL, NULL, NULL, NULL);
+INSERT INTO public.aspsicosocial (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (4, 'ACOMPAÑAMIENTO ESPIRITUAL, FORTALECIMIENTO TEJIDO SOCIAL', '2014-02-14', NULL, NULL, NULL, NULL);
+INSERT INTO public.aspsicosocial (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (5, 'REDES DE APOYO', '2014-02-14', NULL, NULL, NULL, NULL);
+INSERT INTO public.aspsicosocial (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (102, 'PRIMEROS AUXILIOS PSICOLOGICOS', '2014-07-01', NULL, '2014-07-01 15:37:35.991268', '2014-07-01 15:37:35.991268', NULL);
+
+
+INSERT INTO public.emprendimiento (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (0, 'SIN INFORMACIÓN', '2014-02-14', NULL, NULL, NULL, NULL);
+INSERT INTO public.emprendimiento (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (2, 'FORMULACIÓN DE PROYECTO DE EMPRENDIMIENTO', '2014-02-14', NULL, NULL, '2014-08-04 14:16:06.753018', NULL);
+INSERT INTO public.emprendimiento (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (1, 'INFORMACIÓN Y CAPACITACIÓN', '2014-02-14', NULL, NULL, '2014-08-04 14:16:21.864164', NULL);
+INSERT INTO public.emprendimiento (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (5, 'INFORMACIÓN SOBRE INICIATIVA DE EMPRENDIMIENTO', '2014-02-14', NULL, NULL, '2014-08-04 14:17:03.670455', NULL);
+INSERT INTO public.emprendimiento (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (4, 'PAGO DE CUOTA EN CASO DE MICROCRÉDITO', '2014-02-14', NULL, NULL, '2014-08-04 14:17:22.171892', NULL);
+INSERT INTO public.emprendimiento (id, nombre, fechacreacion, fechadeshabilitacion, created_at, updated_at, observaciones) VALUES (6, 'SEGUIMIENTO Y EVALUACIÓN DE PROYECTO E INICIATIVA', '2014-02-14', NULL, NULL, '2014-08-04 14:17:41.68388', NULL);
+
