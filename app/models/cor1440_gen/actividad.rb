a55ec3c473f2bd87096ae205205db3c2d3c5cc96
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-10-11 23:20:16 -0500

    Actualiza infraestructura

--- app/models/cor1440_gen/actividad.rb
+++ app/models/cor1440_gen/actividad.rb
@@ -1,10 +1,10 @@
 # encoding: UTF-8
 
-require 'cor1440_gen/concerns/models/actividad'
+require 'sivel2_sjr/concerns/models/actividad'
 
 module Cor1440Gen
   class Actividad < ActiveRecord::Base
-    include Cor1440Gen::Concerns::Models::Actividad
+    include Sivel2Sjr::Concerns::Models::Actividad
 
     validates :descripcion, length: { maximum: 5000 } 
     validates :metodologia, length: { maximum: 5000 } 
