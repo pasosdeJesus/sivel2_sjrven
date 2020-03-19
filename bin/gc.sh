5712e93dfb750aa2d66e80fef92c6e5e6a1172ba
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-03-19 18:02:57 -0500

    Actualiza

--- bin/gc.sh
+++ bin/gc.sh
@@ -5,49 +5,22 @@ if (test -f ".env") then {
 	. ./.env
 } fi;
 
-grep "^ *gem.*sip.*,.*path:" Gemfile > /dev/null 2> /dev/null
+grep "^[^#]*path" -B 1 -A 1 Gemfile
 if (test "$?" = "0") then {
-	echo "Gemfile incluye un sip cableado al sistema de archivos"
-	exit 1;
-} fi;
-grep "^ *gem.*sivel2_gen.*,.*path:" Gemfile > /dev/null 2> /dev/null
-if (test "$?" = "0") then {
-	echo "Gemfile incluye un sivel2_gen cableado al sistema de archivos"
-	exit 1;
-} fi;
-grep "^ *gem.*sivel2_sjr.*,.*path:" Gemfile > /dev/null 2> /dev/null
-if (test "$?" = "0") then {
-	echo "Gemfile incluye un sivel2_sjr cableado al sistema de archivos"
-	exit 1;
-} fi;
-grep "^ *gem.*cor1440_gen.*,.*path:" Gemfile > /dev/null 2> /dev/null
-if (test "$?" = "0") then {
-	echo "Gemfile incluye un cor1440_gen cableado al sistema de archivos"
-	exit 1;
-} fi;
-grep "^ *gem.*.sal7711_gen.*,.*path:" Gemfile > /dev/null 2> /dev/null
-if (test "$?" = "0") then {
-	echo "Gemfile incluye un sal7711_gen cableado al sistema de archivos"
-	exit 1;
-} fi;
-grep "^ *gem.*.sal7711_web.*,.*path:" Gemfile > /dev/null 2> /dev/null
-if (test "$?" = "0") then {
-	echo "Gemfile incluye un sal7711_web cableado al sistema de archivos"
-	exit 1;
+  echo "Hay una gema cableada en el sistema de archivos"
+  exit 1;
 } fi;
 
-
-grep "^ *gem.*debugger" Gemfile > /dev/null 2> /dev/null
+grep "^ *gem *.debugger*" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
-	echo "Gemfile incluye debugger que heroku no quiere"
+	echo "Gemfile incluye debugger"
 	exit 1;
 } fi;
-grep "^ *gem.*byebug" Gemfile > /dev/null 2> /dev/null
+grep "^ *gem *.byebug*" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
 	echo "Gemfile incluye byebug que rbx de travis-ci no quiere"
 	exit 1;
 } fi;
-
 
 if (test "$SINAC" != "1") then {
 	NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle update
@@ -70,25 +43,32 @@ if (test "$SININS" != "1") then {
 	} fi;
 } fi;
 if (test "$SINMIG" != "1") then {
-	(bundle exec rake db:migrate sip:indices db:structure:dump)
+        PGSSLCERT=$PGSSLCERT PGSSLKEY=$PGSSLKEY bundle exec rake db:migrate sip:indices db:structure:dump
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
 } fi;
 
-RAILS_ENV=test bundle exec rake db:drop db:setup; RAILS_ENV=test bin/rails db:migrate sip:indices
+PGSSLCERT=$PGSSLCERT PGSSLKEY=$PGSSLKEY RAILS_ENV=test bundle exec rake db:drop db:setup;  PGSSLCERT=$PGSSLCERT PGSSLKEY=$PGSSLKEY RAILS_ENV=test bin/rails db:migrate sip:indices
 if (test "$?" != "0") then {
 	echo "No puede preparse base de prueba";
 	exit 1;
 } fi;
 
-RACK_MULTIPART_LIMIT=2048 bundle exec rails test
+PGSSLCERT=$PGSSLCERT PGSSLKEY=$PGSSLKEY bundle exec bin/rails test
 if (test "$?" != "0") then {
 	echo "No pasaron pruebas";
 	exit 1;
 } fi;
 
-RAILS_ENV=test bundle exec rake db:structure:dump
+
+#CONFIG_HOSTS=127.0.0.1 PGSSLCERT=$PGSSLCERT PGSSLKEY=$PGSSLKEY bundle exec bin/rails test:system
+#if (test "$?" != "0") then {
+#	echo "No pasaron pruebas de sistema";
+#	exit 1;
+#} fi;
+
+RAILS_ENV=test bin/rails db:structure:dump
 b=`git branch | grep "^*" | sed -e  "s/^* //g"`
 git status -s
 if (test "$MENSCONS" = "") then {
@@ -101,7 +81,14 @@ if (test "$?" != "0") then {
 	exit 1;
 } fi;
 
+
 if (test "$CONH" != "") then {
 	git push heroku master
 	heroku run rake db:migrate sip:indices
 } fi;
+
+if (test "$?" != "0") then {
+	echo "No puede preparse base de prueba";
+	exit 1;
+} fi;
+
